3c10894d275980e855a6f9cd0b8e1184b903fc8f
Dan Drew <dan.drew@hotmail.com>
2026-02-05 09:09:51 -0800

    fixup! fixup! feat: wry vs dry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import {Component} from '@angular/core';
-import { InputModalComponent } from './common/input-modal/input-modal.component';
-import { RouterOutlet } from '@angular/router';
-import { DebugInfoComponent } from './common/debug-info/debug-info.component';
+import {Component, inject} from '@angular/core';
+import {InputModalComponent} from './common/input-modal/input-modal.component';
+import {RouterOutlet} from '@angular/router';
+import {DebugInfoComponent} from './common/debug-info/debug-info.component';
+import {Match} from "./config/match";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [InputModalComponent, RouterOutlet, DebugInfoComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [InputModalComponent, RouterOutlet, DebugInfoComponent],
+  host: {
+    '[style.--left-team-color]': 'match.profile.teams.leftColor',
+    '[style.--right-team-color]': 'match.profile.teams.rightColor',
+  }
 })
 export class AppComponent {
+  protected readonly match = inject(Match);
 }
